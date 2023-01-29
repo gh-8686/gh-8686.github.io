917a2766faf48db497449365726494d09ec8cbaf
gh-8686 <120648229+gh-8686@users.noreply.github.com>
2023-01-29 18:09:17 +0900

    Add files via upload

---
+++ syoujs/syou.js
@@ -0,0 +1,38 @@
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide'); 
+  });
+
+  close.addEventListener('click' , () =>{
+  overlay.classList.remove('show');
+  open.classList.remove('hide');
+  });  
+}
+
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
+
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      });
+      
+document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
+  });
+}
